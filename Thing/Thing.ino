82b74ef28d70909f92c02b709ffef2b3d510b7e9
Hamish Cunningham <hamish@gate.ac.uk>
2019-10-21 12:22:56 +0100

    week 4

--- Thing/Thing.ino
+++ Thing/Thing.ino
@@ -8,17 +8,27 @@
 
 #include "Ex01.h"
 #include "Ex02.h"
+#include "Ex03.h"
+#include "Ex04.h"
+#include "Ex05.h"
+#include "Ex06.h"
 
-int LABNUM = 1; // which lab exercise number are we doing?
+int LABNUM = 3; // which lab exercise number are we doing?
 
 // function prototypes, needed by newer Arduino IDEs (and IDF)
 void setup01(); void loop01(); void setup02(); void loop02();
+void setup03(); void loop03(); void setup04(); void loop04();
+void setup05(); void loop05(); void setup06(); void loop06();
 
 // initialisation entry point
 void setup() {
   switch(LABNUM) {
     case  1: setup01(); break;
     case  2: setup02(); break;
+    case  3: setup03(); break;
+    case  4: setup04(); break;
+    case  5: setup05(); break;
+    case  6: setup06(); break;
     default: Serial.println("oops! invalid lab number");
   }
 }
@@ -28,6 +38,10 @@ void loop() {
   switch(LABNUM) {
     case  1: loop01(); break;
     case  2: loop02(); break;
+    case  3: loop03(); break;
+    case  4: loop04(); break;
+    case  5: loop05(); break;
+    case  6: loop06(); break;
     default: Serial.println("oops! invalid lab number");
   }
 }
