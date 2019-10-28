71b275db3b20e2c26f7c20929bee8cb73d94cfb2
Hamish Cunningham <hamish@gate.ac.uk>
2019-10-28 11:42:38 +0000

    added week 05

--- MyThing/MyThing.ino
+++ MyThing/MyThing.ino
@@ -1,31 +1,12 @@
 // MyThing.ino
 // COM3505 lab exercises sketch template
 
-int pushButton = 14;
-int externalLED = 32;
-int state = 0;
-int previousMills;
-unsigned long interval = 100;
-
-
 // initialisation entry point
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(pushButton, INPUT_PULLUP);
-  pinMode(externalLED, OUTPUT);
-  Serial.begin(115200);
+  
 }
 
 // task loop entry point
 void loop() {
-  unsigned long currentMills = millis();
-  int button = digitalRead(pushButton);
-  if (currentMills - previousMills >= interval && button == 0) {
-    previousMills = currentMills;
-
-    if (state == 0) { state = 1;}
-    else {state = 0;};
-    
-  }
-  digitalWrite(externalLED, state);
+ 
 }
--- Thing/Thing.ino
+++ Thing/Thing.ino
@@ -12,13 +12,18 @@
 #include "Ex04.h"
 #include "Ex05.h"
 #include "Ex06.h"
+#include "Ex07.h"
+#include "Ex08.h"
+#include "Ex09.h"
 
-int LABNUM = 3; // which lab exercise number are we doing?
+int LABNUM = 9; // which lab exercise number are we doing?
 
 // function prototypes, needed by newer Arduino IDEs (and IDF)
 void setup01(); void loop01(); void setup02(); void loop02();
 void setup03(); void loop03(); void setup04(); void loop04();
 void setup05(); void loop05(); void setup06(); void loop06();
+void setup07(); void loop07(); void setup08(); void loop08();
+void setup09(); void loop09(); 
 
 // initialisation entry point
 void setup() {
@@ -29,6 +34,9 @@ void setup() {
     case  4: setup04(); break;
     case  5: setup05(); break;
     case  6: setup06(); break;
+    case  7: setup07(); break;
+    case  8: setup08(); break;
+    case  9: setup09(); break;
     default: Serial.println("oops! invalid lab number");
   }
 }
@@ -42,6 +50,9 @@ void loop() {
     case  4: loop04(); break;
     case  5: loop05(); break;
     case  6: loop06(); break;
+    case  7: loop07(); break;
+    case  8: loop08(); break;
+    case  9: loop09(); break;
     default: Serial.println("oops! invalid lab number");
   }
 }
