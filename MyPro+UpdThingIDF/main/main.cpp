93975eb501cc6fc7abb64a50792bd4fe8284b05b
Luke Peacock <lukepeacock13@gmail.com>
2019-10-30 15:06:34 +0000

    fixes

--- MyPro+UpdThingIDF/main/main.cpp
+++ MyPro+UpdThingIDF/main/main.cpp
@@ -96,7 +96,7 @@ void setup() {
 
   touch_pad_init();
   touch_pad_set_voltage(TOUCH_HVOLT_2V7, TOUCH_LVOLT_0V5, TOUCH_HVOLT_ATTEN_1V);
-  touch_pad_config(TOUCH_PAD_NUM1, TOUCH_THRES_NO_USE);
+  touch_pad_config(TOUCH_PAD_NUM1, TOUCH_THRESH_NO_USE);
   delay(300); blink(3);         // signal we've finished config
   printf("\n"); delay(500); printf("\n");
 }
