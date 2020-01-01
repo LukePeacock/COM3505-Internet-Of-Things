e13f7aaeb2dc80513e58e97a74ebb411d0618bf4
Rokas <rbagdonas1@sheffield.ac.uk>
2020-01-01 13:20:49 +0000

    refactor radio transmitor to have a separate class
    Refactor because of future usage in telegram.cpp.
    Everthing related to activating switches should now be handled in transmitor.cpp

---
+++ MyProjectThing/sketch/transmitor.cpp
@@ -0,0 +1,29 @@
+#include "transmitor.h"
+
+
+RCSwitch mySwitch = RCSwitch();
+void setupSwitch()
+{
+  mySwitch.enableTransmit(12);    // Transmitter is connected to esp32 Pin #12
+  mySwitch.setPulseLength(175);   // We need to set pulse length as it's different from default
+}
+
+void plug2On()
+{
+  mySwitch.send(1398211, 24);
+}
+
+void plug2Off()
+{
+  mySwitch.send(1398220, 24);      
+}
+
+void plug3On()
+{
+  mySwitch.send(5527299, 24);
+}
+
+void plug3Off()
+{
+  mySwitch.send(5527308, 24);
+}
